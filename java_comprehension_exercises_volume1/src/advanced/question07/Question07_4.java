78c4d558c6dcd81800654706cd80e63cd270428d
kkatsuya <kkatsuya@C010423092.zipangu.cac.co.jp>
2026-04-16 14:49:00 +0900

    question07_4まで終了しました。

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_4.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_4.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_4 {
 
@@ -14,6 +16,23 @@ public class Question07_4 {
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int sum_prices = 0;
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の購入数：＞");
+			amounts[i] = Integer.parseInt(br.readLine());
+			sum_prices += prices[i] * amounts[i];
+		}
+
+		System.out.println("割引前合計：" + sum_prices + "円");
+		if (sum_prices >= 5000) {
+			System.out.println("割引適用（10%OFF）:" + (int) (sum_prices * 0.9) + "円");
+			System.out.println("税込合計：" + (int) (sum_prices * 0.9) * 1.1);
+		} else {
+			System.out.println("税込合計：" + (int) (sum_prices * 1.1) + "円");
+		}
 
 	}
 
